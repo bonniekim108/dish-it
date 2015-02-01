315242aab03e4699303a7075ff45b55125a8e2eb
Bonnie <bonniekim108@yahoo.com>
2015-02-01 12:26:16 -0800

    User model and migration added

--- db/migrate/20150130224416_create_users.rb
+++ db/migrate/20150130224416_create_users.rb
@@ -4,6 +4,8 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.string :password_digest
+      t.has_many :votes, index: true
+      t.has_many :restaurants, index: true
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150131005608) do
+ActiveRecord::Schema.define(version: 20150131005332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,12 +53,6 @@ ActiveRecord::Schema.define(version: 20150131005608) do
   end
 
   create_table "suggestions", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "trash_talks", force: :cascade do |t|
-    t.string   "trash"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
