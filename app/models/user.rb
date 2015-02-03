c4866abe2c908302a3cd588bc3733be493a0a15f
Bonnie <bonniekim108@yahoo.com>
2015-02-02 20:25:15 -0800

    tests added to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  
   has_secure_password
   has_one :county
   has_many :votes
@@ -6,6 +7,13 @@ class User < ActiveRecord::Base
   
   validates :password, length: { minimum: 4 }, allow_nil: true
   validates :password, confirmation: true
-  validates :name, :email, presence: true
+  validates :name, presence: true
+  validates :email, uniqueness: {case_sensitive: false}
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :county, presence: true
 
+  def is_admin?
+    is_admin: false
+  end
 end
+
