05c18ded5e85d4d8a61c135bdf8a049bc968a904
jim-clark <email@jim-clark.com>
2015-02-03 15:45:31 -0800

    add factorygirl for PotentialDish model

--- spec/models/cuisine_lookup_spec.rb
+++ spec/models/cuisine_lookup_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe CuisineLookup, :type => :model do
   it 'has a valid factory' do
     cuisine = build(:cuisine_lookup)
+    expect(cuisine).to be_valid
   end
   it 'is invalid without a name' do
     cuisine = build(:cuisine_lookup, name: nil)
