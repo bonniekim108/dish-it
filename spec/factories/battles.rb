bec384e83bfde8f033fa332fcb700d8ed9b7015c
jim-clark <email@jim-clark.com>
2015-02-04 11:22:29 -0800

    changed battles factory

--- spec/factories/battles.rb
+++ spec/factories/battles.rb
@@ -7,17 +7,17 @@ FactoryGirl.define do
       create_count 5
     end
 
-    after(:create) do |bat, evaluator|
+    after(:build) do |bat, evaluator|
       bat.dish ||= build(:dish, :battle => bat)
-      create_list(:restaurant, evaluator.create_count, battle: bat)
-      create_list(:trash_talk, evaluator.create_count, battle: bat)
+      build_list(:restaurant, evaluator.create_count, battle: bat)
+      build_list(:trash_talk, evaluator.create_count, battle: bat)
     end
 
   end
 
   factory :battle_without_restaurants_or_trash, parent: :battle do |b|
     year_month { Date.new(2015, 2, -1) }
-    after(:create) do |bat, evaluator|
+    after(:build) do |bat, evaluator|
       bat.dish ||= build(:dish, :battle => bat)
     end
   end
