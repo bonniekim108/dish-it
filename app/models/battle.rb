0a0796753373387c40f72f888fe52efe2b7946c6
edwinalegre <edwin_alegre@yahoo.com>
2015-02-04 19:42:20 -0800

    Added Battle model testing

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -3,9 +3,34 @@ class Battle < ActiveRecord::Base
   has_many :restaurants
   has_many :trash_talks
 
-  def display_mode
-  end
-
   validates :year_month, presence: true
   validates :dish, presence: true
+  validate :is_eom
+  validate :display_mode
+
+  def is_eom
+  	if self.year_month
+	  	eom = Date.new(self.year_month.year, self.year_month.month, -1)
+	  	if eom != self.year_month
+	  		errors.add(:year_month, "must be end of month")
+	  	end
+    end
+  end
+
+  def display_mode
+  	if self.year_month
+		countdown = (self.year_month.day - Date.today.day)
+		if Date.today.year < self.year_month.year
+			"future"
+		elsif Date.today.month < self.year_month.month
+	  		"future"
+		elsif countdown > 13
+	  		"nominating"
+	  	elsif countdown < 13 && countdown > 6
+	  		"great_eight"
+	  	elsif countdown > 0
+	  		"final_four"
+	  	end
+    end
+  end
 end
