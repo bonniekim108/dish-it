06cee064d10aa3a96100b925b066fc91c3858fc7
jim-clark <email@jim-clark.com>
2015-02-04 17:26:05 -0800

    more conflict resolution

---
+++ app/assets/javascripts/rest-resources/user-resource.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  angular.module('app')
+  .factory('UserResource', [
+    '$resource',
+    function ($resource) {
+      var Resource = $resource('/api/users', null,
+        {
+          // mapping to standard rails methods
+          // 'index':    {method: 'GET', isArray: true},
+          // 'show':     {method: 'GET', isArray: false},
+          // 'create':   {method: 'POST'},
+          // 'update':   {method: 'PUT'},
+          // other methods in rails UsersController
+          'getUser': {
+                        method: 'GET',
+                        url: '/api/get-user/:token',
+                        params:{token: '@token'},
+                        headers : {'Content-Type' : 'application/json'}
+                      }
+        }
+      );
+
+      // Add custom functions
+
+      return Resource;
+    }
+  ]);
+
+})();
