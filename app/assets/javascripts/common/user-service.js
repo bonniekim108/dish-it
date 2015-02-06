94cd8aa979040ba729c407305f56e4f11a77ac1a
jim-clark <email@jim-clark.com>
2015-02-05 19:26:59 -0800

    Finished UserService

--- app/assets/javascripts/common/user-service.js
+++ app/assets/javascripts/common/user-service.js
@@ -1,62 +1,47 @@
 (function() {
   'use strict';
 
-  angular.module('app') // returns the app module
+  angular.module('app')
 
   .factory('UserService', ['UserResource', function(UserResource) {
-    
+
     var service = {};
 
-    service.currentUser = {};
+    service.getUser = function () {
+      return service.user;
+    };
 
     service.login = function(email, password) {
-
       UserResource.loginEmail({login: {email: email, password: password}}, function(user){
-
-        service.currentUser = user;
-
+        service.user = user;
       }, function(error){ 
-        service.currentUser = null;
-
+        service.user = null;
       });
-
     };
 
     service.logout = function() {
-
       UserResource.logout();
-
+      service.user = null;
     };
 
     service.signup = function(name, email, password, password_confirmation, county) {
-
       UserResource.signup({signup: {name: name, email: email, password: password, password_confirmation: password_confirmation, county: county}}, 
         function(user){
-          service.currentUser = user;
-
+          service.user = user;
         }, function(error) {
-            service.currentUser = null;
+          service.user = null;
         });
     };
 
-
     service.loginToken = function(token) {
-
       UserResource.loginToken({token: token}, function(user){
-
-        service.currentUser = user;
-
+        service.user = user;
       }, function(error){ 
-        service.currentUser = null;
-
+        service.user = null;
       });
-
     };
 
-
-
-
     return service;
-}]);
+  }]);
 
-  })();
+})();
