1f63ccddc42e8057beedec2773706102cc115d00
jim-clark <email@jim-clark.com>
2015-02-05 17:21:32 -0800

    modified seed and factory

--- spec/factories/counties.rb
+++ spec/factories/counties.rb
@@ -3,15 +3,6 @@ FactoryGirl.define do
   factory :county do
     name 'Los Angeles'
     UTC_offset -8
-
-    transient do
-      users_count 5
-    end
-
-    after(:create) do |cnty, evaluator|
-      create_list(:user, evaluator.users_count, county: cnty)
-    end
-
   end
 
 end
