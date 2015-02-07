6c6de0305bb53ddebdac3cfcc8dd8dbb6a3cb66f
jim-clark <email@jim-clark.com>
2015-02-07 14:06:59 -0800

    added a let for cur_month and cur_year in battles_controller_spec

--- spec/controllers/battles_controller_spec.rb
+++ spec/controllers/battles_controller_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe BattlesController, :type => :controller do
+  let(:cur_year) { Date.today.year }
+  let(:cur_month) { Date.today.month }
 
   describe "GET #show" do
     let(:expected_results) { Battle.to_json }
 
     it "returns json" do
-      get :show, {year: 2015, month: 1}
+      get :show, {year: cur_year, month: cur_month}
       expect {JSON.parse(response.body)}.to_not raise_error
     end
 
