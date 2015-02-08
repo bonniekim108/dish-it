ed010030c532cca1d34a0501e31cc3016c5e9d8c
jim-clark <email@jim-clark.com>
2015-02-07 17:35:26 -0800

    added create method to battles_controller using TDD

--- spec/controllers/battles_controller_spec.rb
+++ spec/controllers/battles_controller_spec.rb
@@ -14,4 +14,17 @@ RSpec.describe BattlesController, :type => :controller do
 
   end
 
+  describe "POST #create" do
+    let(:required_params) { { year: cur_year + 1, month: cur_month, dish_name: 'Test Dish Name', dish_cuisine: 'Test Cuisine' } }
+
+    it "creates a new battle with the required params" do
+      post :create, battle: required_params
+      battle = JSON.parse(response.body)
+      expect( battle['year_month'].to_date ).to eq( Date.new(cur_year + 1, cur_month, -1) )
+      expect( battle['dish'].to_json ).to eq( {name: 'Test Dish Name', cuisine: 'Test Cuisine'}.to_json )
+    end
+
+  end
+
+
 end
