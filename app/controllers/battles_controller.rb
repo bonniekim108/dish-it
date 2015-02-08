91d2f0c3c24fddf93bb03000cb2443980ee759c0
jim-clark <email@jim-clark.com>
2015-02-08 13:25:23 -0800

    added upvote method to rails battles_controller using TDD

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -26,6 +26,7 @@ class BattlesController < ApplicationController
 
   def create
     begin
+      raise error if !current_user
       date = Date.new(params[:battle][:year].to_i, params[:battle][:month].to_i, -1)
       @battle = Battle.create(year_month: date, dish: Dish.new(name: params[:battle][:dish_name], cuisine: params[:battle][:dish_cuisine]))
       render json: @battle, include: {
@@ -38,5 +39,24 @@ class BattlesController < ApplicationController
       render plain: 'Battle not created', status: :bad_request
     end      
   end
+
+  def upvote
+    begin
+      user = current_user
+      raise error if !user
+      @battle = Battle.find_by(year_month: Date.today.end_of_month)
+      @battle.restaurants.find(params[:vote][:restaurant_id]).votes << Vote.new(user: user, comment: params[:vote][:comment])
+      @battle.save
+      render json: @battle, include: {
+        dish: { only: [ :cuisine, :name ] },
+        trash_talks: { include: {user: { only: :name } }, only: [ :created_at, :user, :trash ] },
+        restaurants: { include: {votes: { include: { user: { only: :name } }, only: [ :created_at, :comment, :user ] } } }
+        },
+        status: :ok
+    rescue
+      render plain: 'Upvote not registered', status: :bad_request
+    end      
+  end
+
 
 end
