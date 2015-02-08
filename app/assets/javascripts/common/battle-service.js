504966d8d8699eef9ad60f10b438894e807e9017
jim-clark <email@jim-clark.com>
2015-02-08 14:13:45 -0800

    refactored ng BattleService to sort a battle's restaurants by it's number of votes desc

--- app/assets/javascripts/common/battle-service.js
+++ app/assets/javascripts/common/battle-service.js
@@ -11,34 +11,42 @@
 
 		service.currentBattle = function(){
 			var deferred = $q.defer();
-			if ( curBattle ) {
+			if (curBattle) {
 				return curBattle;
 			} else {
 				var today = new Date();
 				var year = today.getFullYear();
 				var month = today.getMonth() + 1;
-				BattleResource.show({
-					year: year, month: month
-				},function(battle){
-					curBattle = battle;
-					deferred.resolve(curBattle);
-				},function(fail){
-					deferred.reject(fail);
-				})
+				getSortedBattle(year, month).then(
+					function (battle) {
+						deferred.resolve(battle);
+					},
+					function (fail) {
+						deferred.reject(fail);
+					}
+				)
 			}
 			return deferred.promise;
 		};
 
 
+		/*  Private Functions  */
 
-
-
-
-
-
-
-
-
+		function getSortedBattle(yr, mo) {
+			var deferred = $q.defer();
+			BattleResource.show({
+					year: yr, month: mo
+				}, function (battle) {
+					var sorted = _.sortBy(battle.restaurants, function(r) {
+						return r.votes.length * -1;
+					});
+					battle.restaurants = sorted;
+					deferred.resolve(battle);
+				}, function (fail) {
+					deferred.reject(fail);
+				});
+			return deferred.promise;
+		}
 
 
 		return service;
