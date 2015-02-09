1e97c472042ab56d4ed9259f40e90c29b979f9dd
jim-clark <email@jim-clark.com>
2015-02-09 05:50:20 -0800

    finished ng YelpService

--- app/assets/javascripts/common/yelp-service.js
+++ app/assets/javascripts/common/yelp-service.js
@@ -3,7 +3,7 @@
 
   angular.module('app')
 
-  .factory('YelpService', ['$http', function($http){
+  .factory('YelpService', ['$http', '$q', 'UserService', function($http, $q, UserService){
 
     var service = {};
 
@@ -15,44 +15,43 @@
 
     // be sure to replace spaces in injected name & city with +'s
     var baseUrl = 'http://api.yelp.com/v2/search';
-    var optionalLocation = '&location={city}';
 
     var callCount = 0;
+    var pending;
 
-
-    service.search = function (name, city, callback) {
+    service.search = function (name, callback) {
+      if (pending) {
+        pending.resolve('cancelled by user');
+      }
+      pending = $q.defer();
       var method = 'GET';
       var url = 'http://api.yelp.com/v2/search';
       var params = {
         callback: 'angular.callbacks._' + callCount,
-        location: city,
+        category_filter: 'restaurants',
+        location: getCounty(),
         oauth_consumer_key: consumerKey,
         oauth_token: token,
         oauth_signature_method: 'HMAC-SHA1',
         oauth_timestamp: new Date().getTime(),
         oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+        limit: 10,
         term: name
       };
       var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, { encodeSignature: false});
-      params['oauth_signature'] = signature;
+      params.oauth_signature = signature;
       callCount++;
-      $http.jsonp(url, {params: params}).success(callback);
-
+      $http.jsonp(url, {params: params, timeout: pending.promise})
+        .success(callback);
     };
-
-
-    service.search('food+yardhouse', 'los+angeles', function(data){
-      console.log(data);
-    });
-    service.search('restaurants+sharkys', 'west+covina', function(data){
-      console.log(data);
-    });
-
-
 
     return service;
 
     /*  private functions  */
+
+    function getCounty () {
+      return UserService.getUser().county.name + ' County';
+    }
 
     function randomString(length, chars) {
       var result = '';
