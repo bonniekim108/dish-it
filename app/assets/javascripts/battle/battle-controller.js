a428a84848a1f8c144932741661d930fa4de0810
jim-clark <email@jim-clark.com>
2015-02-13 09:40:50 -0800

    added demo mode in footer

--- app/assets/javascripts/battle/battle-controller.js
+++ app/assets/javascripts/battle/battle-controller.js
@@ -4,17 +4,22 @@
   angular.module('app')
 
   .controller('BattleController', [
+    '$rootScope',
     'BattleService',
-    function(BattleService) {
+    function($rootScope, BattleService) {
       var vm = this;
 
       vm.curBattle = BattleService.curBattle;
-      vm.displayMode = BattleService.displayMode; 
+      vm.displayMode = BattleService.getDisplayMode(); 
+
+      // demo mode code
+      $rootScope.$on('demo-mode-changed', function () {
+        vm.displayMode = BattleService.getDisplayMode();
+      });
 
       vm.userCanVote = function () {
         return BattleService.userCanVote();
       };
-
 
       vm.displayLimit = function () {
         if (vm.displayMode == 'nominating') {
