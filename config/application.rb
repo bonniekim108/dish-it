dd2bfd5a1a5c67c7fd407bf87f94edb1418e76c5
jim-clark <email@jim-clark.com>
2015-02-14 13:16:24 -0800

    fix appication.rb syntax error

--- config/application.rb
+++ config/application.rb
@@ -14,9 +14,6 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# added to serve images from public folder
-config.serve_static_assets = true
-
 module RailsAngularPostgresql
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
