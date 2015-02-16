7ec56683bf0d97d443f91775ed90be163f9a1b5b
jim-clark <email@jim-clark.com>
2015-02-16 04:27:11 -0800

    remove console.logs

--- app/assets/javascripts/winners/winners-controller.js
+++ app/assets/javascripts/winners/winners-controller.js
@@ -11,7 +11,6 @@
       var vm = this;
 
       vm.winners = BattleService.winners;
-      console.log($stateParams);
       vm.restaurantId = $stateParams.id;
       vm.battleId = $stateParams.battle_id;
       vm.restaurant = {};
@@ -20,11 +19,9 @@
       for (var i = 0; i < vm.winners.length; i++){
         if (vm.winners[i].id == vm.battleId) {
           vm.battle = vm.winners[i];
-              console.log(vm.battle.restaurants.length);
           for (var j = 0; j < vm.battle.restaurants.length; j++){
             if (vm.battle.restaurants[j].id == vm.restaurantId) {
               vm.restaurant = vm.battle.restaurants[j];
-              console.log(vm.restaurant);
             }
           }
         }
